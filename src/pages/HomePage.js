1b424580aaec28e8bb64bdf58979b5a4c6f7b344
B200910002 <jamtsjak123@gmail.com>
2022-04-20 19:15:43 +0800

    homePAge

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,10 +4,10 @@ class HomePage extends Component {
   constructor(props) {
     super(props);
   }
-  state = {  }
-  render() { 
-    return (  );
+  state = {};
+  render() {
+    return <div></div>;
   }
 }
- 
+
 export default HomePage;
